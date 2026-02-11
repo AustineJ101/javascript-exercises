89b13f10497d145b00fbb551658f825e06a62181
Austine Juma <austinejumah001@gmail.com>
2026-02-11 23:54:13 +0300

    Complete fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num){
+    let fib = [1,1,2]
+    if(num == 0) return 0;
+    if(num < 0) return "OOPS";
+    if(num <= 3){
+        return fib[num -1];
+    }else{
+        for(let i = 4; i <= num; i++){
+            let nextFibNum = fib[i - 3] + fib[i - 2];
+            fib.push(nextFibNum);
+        }
 
+        return fib.at(-1);
+    }
+    
 };
 
 // Do not edit below this line
