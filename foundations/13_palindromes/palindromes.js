5cef290b865d56014f27a3df354cb862fc265fd4
Austine Juma <austinejumah001@gmail.com>
2026-02-11 23:31:02 +0300

    Implement palindromee checker

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let cleanStr = removePunctuation(str).split(" ").join("").toLowerCase();
 
+    let comparStr = cleanStr.split("").reverse().join("");
+
+    return cleanStr == comparStr;
 };
+
+function removePunctuation(str) {
+    return str.split('').filter(char => {
+        return /[a-zA-Z0-9 ]/.test(char);
+    }).join('');
+}
+
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
