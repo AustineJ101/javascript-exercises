2e672bce19dc32db4f598bcf06ef120083c08e1b
Austine Juma <austinejumah001@gmail.com>
2026-02-12 00:23:22 +0300

    complete the sorting exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people){
+    let currentYear = new Date().getFullYear();
+
+    for(let person of people){
+        if(person.yearOfDeath == undefined){
+            person["age"] = currentYear - person.yearOfBirth;
+        }else{
+            person["age"] = person.yearOfDeath - person.yearOfBirth;
+        }
+    }
+
+    people.sort((a,b) => b.age - a.age);
+
+    return people[0];
 
 };
 
